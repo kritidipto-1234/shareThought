51a33649086444cb3d1b0b459008234832717477
Kritidipto Ghosh <kritidiptodgp.ghosh@gmail.com>
2021-07-18 13:52:04 +0530

    some changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,43 @@
-import '@babel/polyfill';
+import "@babel/polyfill";
 
-import * as landingPage from './landingPage.js';
-import * as globalPage from './globalPage.js';
-import * as resetPasswordPage from './resetPasswordPage.js';
-import * as myAccountPage from './myAccountPage.js';
-import * as userProfilePage from './userProfilePage.js';
+import * as landingPage from "./landingPage.js";
+import * as globalPage from "./globalPage.js";
+import * as resetPasswordPage from "./resetPasswordPage.js";
+import * as myAccountPage from "./myAccountPage.js";
+import * as userProfilePage from "./userProfilePage.js";
 
-if (document.querySelector('.contentContainer').dataset.pagename==='Landing Page') 
+var viewport = document.querySelector("meta[name=viewport]");
+viewport.setAttribute(
+    "content",
+    viewport.content + ", height=" + window.innerHeight
+);
+
+if (
+    document.querySelector(".contentContainer").dataset.pagename ===
+    "Landing Page"
+)
     landingPage.init();
 
-if (document.querySelector('.contentContainer').dataset.pagename==='Global Page') 
+if (
+    document.querySelector(".contentContainer").dataset.pagename ===
+    "Global Page"
+)
     globalPage.init();
 
-if (document.querySelector('.contentContainer').dataset.pagename==='Change Password Page') 
+if (
+    document.querySelector(".contentContainer").dataset.pagename ===
+    "Change Password Page"
+)
     resetPasswordPage.init();
 
-if (document.querySelector('.contentContainer').dataset.pagename==='My Account Page') 
+if (
+    document.querySelector(".contentContainer").dataset.pagename ===
+    "My Account Page"
+)
     myAccountPage.init();
 
-if (document.querySelector('.contentContainer').dataset.pagename==='User Profile Page') 
+if (
+    document.querySelector(".contentContainer").dataset.pagename ===
+    "User Profile Page"
+)
     userProfilePage.init();
